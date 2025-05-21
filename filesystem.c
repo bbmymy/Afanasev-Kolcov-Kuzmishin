f648fc6d30c3423394e325c5985529bc44ccf3f1
bbmymy <www.afanas.oleg@gmail.com>
2025-05-21 12:57:05 +0300

    Update filesystem.c

--- filesystem.c
+++ filesystem.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h> // Для mkstemp
 
 #define MAX_SIZE 1024
 
@@ -102,4 +103,74 @@ int delete_file_in_fs(FILE** fs, const char* filename, const char* fs_filename) 
     
     *fs = fopen(fs_filename, "r+");
     return deleted;
+}
+
+// 2.1 Функция создания нового файла
+int create_new_file(FILE* fs, const char* filename, const char* content) {
+    fseek(fs, 0, SEEK_END); // Перемещаемся в конец файла
+    
+    // Проверяем, не существует ли уже файл с таким именем
+    fseek(fs, 0, SEEK_SET);
+    char buffer[MAX_SIZE];
+    while (fgets(buffer, MAX_SIZE, fs) != NULL) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        if (strcmp(buffer, filename) == 0) {
+            fprintf(stderr, "File already exists\n");
+            return 0;
+        }
+    }
+    
+    // Записываем имя файла и содержимое
+    if (fprintf(fs, "%s\n%s\n/\n", filename, content) < 0) {
+        return 0;
+    }
+    
+    return 1;
+}
+
+// 2.2 Функция изменения файла
+int modify_file_in_fs(FILE** fs, const char* filename, const char* new_content, const char* fs_filename) {
+    fseek(*fs, 0, SEEK_SET);
+    
+    char temp_filename[] = "temp_fs.XXXXXX";
+    int fd = mkstemp(temp_filename);
+    FILE* temp_fs = fdopen(fd, "w+");
+    
+    if (temp_fs == NULL) {
+        perror("Failed to create temporary filesystem");
+        return 0;
+    }
+    
+    char buffer[MAX_SIZE];
+    int modify_mode = 0;
+    int modified = 0;
+    
+    while (fgets(buffer, MAX_SIZE, *fs) != NULL) {
+        buffer[strcspn(buffer, "\n")] = 0;
+        
+        if (!modify_mode) {
+            if (strcmp(buffer, filename) == 0) {
+                modify_mode = 1;
+                modified = 1;
+                fprintf(temp_fs, "%s\n", buffer); // Сохраняем имя файла
+                fprintf(temp_fs, "%s\n", new_content); // Записываем новое содержимое
+                continue;
+            }
+            fprintf(temp_fs, "%s\n", buffer);
+        } else {
+            if (buffer[0] == '/') {
+                modify_mode = 0;
+                fprintf(temp_fs, "/\n");
+            }
+        }
+    }
+    
+    fclose(*fs);
+    fclose(temp_fs);
+    
+    remove(fs_filename);
+    rename(temp_filename, fs_filename);
+    
+    *fs = fopen(fs_filename, "r+");
+    return modified;
 }
